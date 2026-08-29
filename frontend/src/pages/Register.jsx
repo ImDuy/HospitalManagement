0eb977793a5504a0ea6a1cabadba220f2128872f
ImDuy <158559509+ImDuy@users.noreply.github.com>
2026-08-29 20:32:49 +1000

    SCRUM-22: Navigate to login

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -34,7 +34,7 @@ const Register = () => {
       if (userData.role === "patient") delete userData.specialty;
       await axiosInstance.post("/api/auth/register", formData);
       alert("Registration successful. Please log in.");
-      // navigate("/login");
+      navigate("/login");
     } catch (error) {
       alert(
         `${error.response?.data?.message ?? "Registration failed."} Please try again.`,
