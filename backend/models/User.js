190be70985f3a207695b31f7d072d60717234dfe
ImDuy <158559509+ImDuy@users.noreply.github.com>
2026-08-29 20:15:37 +1000

    SCRUM-20: Adjust backend to handle user roles and specialty

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,19 +1,24 @@
-
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    university: { type: String },
-    address: { type: String },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["patient", "doctor"], required: true },
+  specialty: {
+    type: String,
+    enum: ["Cardiology", "General Medicine", "Dermatology"],
+    required: function () {
+      return this.role === "doctor";
+    },
+  },
 });
 
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
